1be09006440ff00ba695ba0c5d9ce4a339209c45
Bruna Oliveira <71950727+bru-oliveirax@users.noreply.github.com>
2022-07-24 12:40:31 -0300

    saving data to sd card

--- main_code.cpp
+++ main_code.cpp
@@ -1,7 +1,7 @@
 // Ankaa Project - LASC 2022
 // Thermal Analysis Mission - Payload
 // Thais Schmidt
-// —---------------------------------------
+// —-------------------------------------
  
  
 #include <SdFat.h>
@@ -16,15 +16,17 @@ int CS1 = 5;
 int CS2 = 22;
 int CS3 = 1;
 int SCK = 23;
-MAX6675 sensor1(SCK, CS1, SO);
-MAX6675 sensor2(SCK, CS2, SO);
-MAX6675 sensor3(SCK, CS3, SO);
+1MAX6675 sensor1(SCK, CS1, SO);
+2MAX6675 sensor2(SCK, CS2, SO);
+3MAX6675 sensor3(SCK, CS3, SO);
  
 void setup()
 {
   pinMode(A5, INPUT);
+ 
   // Inicializa o modulo SD
   if(!sdCard.begin(chipSelect,SPI_HALF_SPEED))sdCard.initErrorHalt();
+ 
   // Abre o arquivo LER_POT.TXT
   if (!meuArquivo.open("ler_pot.txt", O_RDWR | O_CREAT | O_AT_END))
   {
@@ -40,9 +42,7 @@ void setup()
 }
 void loop()
 { 
-  Serial.print("Graus C = ");
-  Serial.print("Bulkhead = ",sensor1.readCelsius());
-  Serial.print("Fuselage = ",sensor2.readCelsius());
-  Serial.print("Boattail = ",sensor3.readCelsius());
-  delay(500);
+  message = “Bulkhead: ” + sensor1.readCelsius() + “\nFuselage: ” + sensor2.readCelsius() + “\nBoattail:” + sensor3.readCelsius();
+  meuArquivo.println(message);
+  delay(50);
 }
